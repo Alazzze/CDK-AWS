e29f51e2964b1fba99d773fe7ca87e4d4c413c5d
Alazzze <Alazze91@gmail.com>
2024-03-28 11:46:06 +0200

    update myscript

--- myscript.py
+++ myscript.py
@@ -7,20 +7,20 @@ class MyStack(Stack):
     def __init__(self, scope, id, **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
 
-        # Створення віртуальної приватної хмарної мережі (VPC)
+        # Create a virtual private cloud network (VPC)
         vpc = ec2.Vpc(self, 'MyVpc', max_azs=2)
 
-        # Створення EC2 інстансу
+        # Creating an EC2 instance
         instance = ec2.Instance(self, 'MyInstance',
             instance_type=ec2.InstanceType.of(ec2.InstanceClass.BURSTABLE2, ec2.InstanceSize.MICRO),
             machine_image=ec2.AmazonLinuxImage(),
             vpc=vpc
         )
 
-        # Створення S3 бакету
+        # Creating an S3 bucket
         bucket = s3.Bucket(self, 'MyBucket')
 
-        # Виведення інформації про IP адресу інстансу
+        # Output information about the IP address of the instance
         CfnOutput(self, 'InstancePublicIp', value=instance.instance_public_ip)
 
 app = App()
