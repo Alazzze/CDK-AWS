1ca80371fc8c4b0c63cab84cdd6aa20b1d81a4bb
Stanislav <90621851+Alazzze@users.noreply.github.com>
2024-03-28 15:55:31 +0200

    Update myscript.py

--- myscript.py
+++ myscript.py
@@ -1,14 +1,14 @@
-from aws_cdk import App, Stack, CfnOutput
+from aws_cdk import core
 from aws_cdk import aws_ec2 as ec2
 from aws_cdk import aws_s3 as s3
 
-class MyStack(Stack):
+class MyStack(core.Stack):
 
-    def __init__(self, scope, id, **kwargs) -> None:
+    def __init__(self, scope: core.Construct, id: str, **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
 
         # Create a virtual private cloud network (VPC)
-        vpc = ec2.Vpc(self, 'MyVpc', max_azs=2)
+        vpc = ec2.Vpc(self, 'MyVpc', max_azs=2, region="us-east-1")
 
         # Creating an EC2 instance
         instance = ec2.Instance(self, 'MyInstance',
@@ -19,11 +19,10 @@ class MyStack(Stack):
         )
 
         # Creating an S3 bucket
-        bucket = s3.Bucket(self, 'MyBucket')
+        bucket = s3.Bucket(self, 'MyBucket',
+            removal_policy=core.RemovalPolicy.DESTROY
+        )
 
-        # Output information about the IP address of the instance
-        CfnOutput(self, 'InstancePublicIp', value=instance.instance_public_ip)
-
-app = App()
+app = core.App()
 MyStack(app, "MyStack")
 app.synth()
